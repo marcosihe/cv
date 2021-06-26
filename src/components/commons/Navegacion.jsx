75151c5e861a1930b60b04a188bfb7cb41b89eea
marcosihe <marcosherrera.ib@gmail.com>
2021-06-26 18:41:32 -0300

    Se agrego margenes a los elementos del nav y un espaciado hacia abajo entre en el nav y el componente Sobremi.

--- src/components/commons/Navegacion.jsx
+++ src/components/commons/Navegacion.jsx
@@ -4,45 +4,51 @@ import { NavLink } from "react-router-dom";
 
 export const Navegacion = () => {
   return (
-    <Navbar bg="dark" expand="lg" variant="dark" fixed="top">
-      <Navbar.Brand href="#home" className="ms-3">
-        Marcos E. Herrera
-      </Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="ms-auto">
-          <NavLink exact={true} to="/marcos" className="nav-link">
-            Sobre mí
-          </NavLink>
-          <NavLink exact={true} to="/marcos/educacion" className="nav-link">
-            Educación
-          </NavLink>
-          <NavLink exact={true} to="/marcos/habilidades" className="nav-link">
-            Habilidades
-          </NavLink>
-          <NavLink
-            exact={true}
-            to="/marcos/experiencialaboral"
-            className="nav-link"
-          >
-            Experiencia Laboral
-          </NavLink>
-          <NavLink exact={true} to="/marcos/cursos" className="nav-link">
-            Cursos
-          </NavLink>
-          <NavLink exact={true} to="/marcos/portfolio" className="nav-link">
-            Portfolio
-          </NavLink>
-          <NavLink exact={true} to="/marcos/contacto" className="nav-link">
-            Contacto
-          </NavLink>
-          <NavDropdown title="Idioma" id="basic-nav-dropdown">
-            <NavDropdown.Item href="#action/3.1">Español</NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item href="#action/3.2">Inglés</NavDropdown.Item>
-          </NavDropdown>
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
+    <div>
+      <Navbar bg="dark" expand="lg" variant="dark" fixed="top">
+        <Navbar.Brand href="#home" className="ms-3">
+          Marcos E. Herrera
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+            <NavLink exact={true} to="/marcos" className="nav-link ms-1">
+              Sobre mí
+            </NavLink>
+            <NavLink exact={true} to="/marcos/educacion" className="nav-link ms-1">
+              Educación
+            </NavLink>
+            <NavLink exact={true} to="/marcos/habilidades" className="nav-link ms-1">
+              Habilidades
+            </NavLink>
+            <NavLink
+              exact={true}
+              to="/marcos/experiencialaboral"
+              className="nav-link ms-1"
+            >
+              Experiencia Laboral
+            </NavLink>
+            <NavLink exact={true} to="/marcos/cursos" className="nav-link ms-1">
+              Cursos
+            </NavLink>
+            <NavLink exact={true} to="/marcos/portfolio" className="nav-link ms-1">
+              Portfolio
+            </NavLink>
+            <NavLink exact={true} to="/marcos/contacto" className="nav-link ms-1 me-3">
+              Contacto
+            </NavLink>
+            {/** 
+             <NavDropdown title="Idioma" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Español</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.2">Inglés</NavDropdown.Item>
+            </NavDropdown>
+            */}
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+      <br />
+      <br />
+    </div>
   );
 };
