b4eb08a8f0e38dae6ed0557174783df8d5abac67
marcosihe <marcosherrera.ib@gmail.com>
2021-06-26 23:11:25 -0300

    Se crearon los componentes necesarios para modelar un esquema base correspondiente a la seccion de experiencia laboral.

--- src/components/Experiencia.jsx
+++ src/components/Experiencia.jsx
@@ -1,9 +1,62 @@
-import React from 'react'
+import React from "react";
+import { JobCard } from "./JobCard";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTools, faHandshake, faOilCan, faMicroscope, faPhoneSquare, faLaptopMedical } from "@fortawesome/free-solid-svg-icons";
 
 export const Experiencia = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+  const info = [
+    {
+      title: "Bioingeniería",
+      subtitle: "Sanatorio 9 de Julio S.A.",
+      textBody:
+        "Mantenimientos preventivo y correctivo en el área de Bioingeniería del Sanatorio.",
+      footerText: "Tucumán-Argentina",
+    },
+    {
+      title: "Operario en Estación de Servicio YPF",
+      subtitle: "Petronorte S.A.",
+      textBody: "Consultor interno y asesor externo con orientación al cliente.",
+      footerText: "Tucumán - Argentina",
+    },
+    {
+      title: "Servicio Técnico",
+      subtitle: "Freelancer - Taller Propio",
+      textBody: "Reperación y mantenimiento de determinados equipos médicos",
+      footerText: "Tucumán - Argentina",
+    },
+    {
+      title: "Tutor estudiantil",
+      subtitle: "Facultad de Ciencias Exactas y Tecnología",
+      textBody:
+        "Tutor Par. Miembro del equipo de trabajo de la Tutoría estudiantil de la FACET",
+      footerText: "U.N.T.",
+    },
+    {
+      title: "Ayudante de Cátedra",
+      subtitle: "Facultad de Bioquímica, Química y Farmacia",
+      textBody:
+        "Auxiliar de 2º categoria de la materia Física I. Consultor de trabajos prácticos y ayudante de laboratorio.",
+      footerText: "U.N.T.",
+    },
+    {
+      title: "Recepción de llamadas de emergencias",
+      subtitle: "Ministerio de Seguridad Ciudadana",
+      textBody: "Recepción de llamadas de la línea de emergencia 911.",
+      footerText: "Tucumán - Argentina",
+    },
+  ];
+
+  return (
+    <div>
+      <h2 className="text-center my-5">Experiencia Laboral</h2>
+      <div className="row">
+      <div className="col-sm-12 col-md-4 my-3"><JobCard info={info[0]} icon={<FontAwesomeIcon icon={faLaptopMedical} size="3x" className="text-success"></FontAwesomeIcon>}></JobCard></div>
+      <div className="col-sm-12 col-md-4 my-3"><JobCard info={info[1]} icon={<FontAwesomeIcon icon={faOilCan} size="3x" className="text-secondary"></FontAwesomeIcon>}></JobCard></div>
+      <div className="col-sm-12 col-md-4 my-3"><JobCard info={info[2]} icon={<FontAwesomeIcon icon={faTools} size="3x" className="text-dark"></FontAwesomeIcon>}></JobCard></div>
+      <div className="col-sm-12 col-md-4 my-3"><JobCard info={info[3]} icon={<FontAwesomeIcon icon={faHandshake} size="3x" className="text-danger"></FontAwesomeIcon>}></JobCard></div>
+      <div className="col-sm-12 col-md-4 my-3"><JobCard info={info[4]} icon={<FontAwesomeIcon icon={faMicroscope} size="3x" className="text-warning"></FontAwesomeIcon>}></JobCard></div>
+      <div className="col-sm-12 col-md-4 my-3"><JobCard info={info[5]} icon={<FontAwesomeIcon icon={faPhoneSquare} size="3x" className="text-primary"></FontAwesomeIcon>}></JobCard></div>
+      </div>
+    </div>
+  );
+};
