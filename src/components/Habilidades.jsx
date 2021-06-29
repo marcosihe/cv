032f6a73749d2fb36d4e5e5b215e03bba75d3ca9
marcosihe <marcosherrera.ib@gmail.com>
2021-06-29 11:16:43 -0300

    Se agregaron cambios al componente Habilidades y se incluyeron iconos en la carpeta public para ser usados como links a redes sociales.

--- src/components/Habilidades.jsx
+++ src/components/Habilidades.jsx
@@ -9,63 +9,66 @@ import {
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFistRaised, faLightbulb } from "@fortawesome/free-solid-svg-icons";
 
+
 export const Habilidades = () => {
   return (
     <Fragment>
       <CardColumns className="my-5">
-            <Card.Title>
-              <h2 className="text-center">Habilidades</h2>
-            </Card.Title>
+        <Card.Title>
+          <h2 className="text-center">Habilidades</h2>
+        </Card.Title>
         <CardGroup className="my-3">
           <Card>
-          <Card.Body>
-            <Card.Text className="fw-bold">
+            <Card.Body>
+              <Card.Text className="fw-bold">
                 <div>
-                    <ProgressBar animated now={95} variant="success" />
-                    <p>HTML5</p>
+                  <ProgressBar animated now={95} variant="success" />
+                  <p>HTML5</p>
                 </div>
                 <div>
-                    <ProgressBar animated now={70} variant="success" />
-                    <p>CSS3</p>
+                  <ProgressBar animated now={70} variant="success" />
+                  <p>CSS3</p>
                 </div>
                 <div>
-                    <ProgressBar animated now={80} variant="success" />
-                    <p>Javascript</p>
+                  <ProgressBar animated now={80} variant="success" />
+                  <p>Javascript</p>
                 </div>
                 <div>
-                    <ProgressBar animated now={60} variant="success" />
-                    <p>React</p>
+                  <ProgressBar animated now={60} variant="success" />
+                  <p>React</p>
                 </div>
                 <div>
-                    <ProgressBar animated now={60} variant="success" />
-                    <p>Mongo DB</p>
+                  <ProgressBar animated now={60} variant="success" />
+                  <p>Mongo DB</p>
                 </div>
                 <div>
-                    <ProgressBar animated now={60} variant="success" />
-                    <p>Node js</p>
+                  <ProgressBar animated now={60} variant="success" />
+                  <p>Node js</p>
                 </div>
-            </Card.Text>
-          </Card.Body>
+              </Card.Text>
+            </Card.Body>
           </Card>
           <Card>
             <Card.Body>
-            <Card.Title className="text-center">Conocimientos intermedios en...</Card.Title>
-            <Card.Text>
+              <Card.Title className="text-center">
+                Conocimientos intermedios en...
+              </Card.Title>
+              <Card.Text>
                 <ListGroup className="fw-bold">
-                    <ListGroup.Item variant="warning">Java</ListGroup.Item>
-                    <ListGroup.Item variant="dark">MySQL</ListGroup.Item>
-                    <ListGroup.Item variant="warning">Lenguaje C</ListGroup.Item>
-                    <ListGroup.Item variant="dark">C# & .NET</ListGroup.Item>
-                    <ListGroup.Item variant="warning">Dart</ListGroup.Item>
-                    <ListGroup.Item variant="dark">Flutter</ListGroup.Item>
-                    <ListGroup.Item variant="warning">Bootstrap</ListGroup.Item>
-                    <ListGroup.Item variant="dark">Git</ListGroup.Item>
+                  <ListGroup.Item variant="warning">Java</ListGroup.Item>
+                  <ListGroup.Item variant="dark">MySQL</ListGroup.Item>
+                  <ListGroup.Item variant="warning">Lenguaje C</ListGroup.Item>
+                  <ListGroup.Item variant="dark">C# & .NET</ListGroup.Item>
+                  <ListGroup.Item variant="warning">Dart</ListGroup.Item>
+                  <ListGroup.Item variant="dark">Flutter</ListGroup.Item>
+                  <ListGroup.Item variant="warning">Bootstrap</ListGroup.Item>
+                  <ListGroup.Item variant="dark">Git</ListGroup.Item>
                 </ListGroup>
-            </Card.Text>
+              </Card.Text>
             </Card.Body>
-        </Card>
+          </Card>
         </CardGroup>
-        
+
         <CardGroup>
           <Card>
             <Card.Body className="text-center">
@@ -91,8 +94,8 @@ export const Habilidades = () => {
                   Capacidad para entender a personas que hablan en idioma Inglés
                 </ListGroup.Item>
                 <ListGroup.Item>
-                  Habilidad para comunicar, paciencia y predisposición para
-                  ayudar
+                  Habilidad para comunicar, empatía, paciencia y predisposición
+                  para ayudar
                 </ListGroup.Item>
               </ListGroup>
             </Card.Text>
@@ -108,19 +111,21 @@ export const Habilidades = () => {
             </Card.Body>
             <Card.Text>
               <ListGroup variant="flush" className="">
-                  <ListGroup.Item>
-                    Sin experiencia formal con empresas de programación
-                  </ListGroup.Item>
                 <ListGroup.Item>
                   Nivel intermedio de Inglés para expresar ideas
                 </ListGroup.Item>
                 <ListGroup.Item>
-                  Sobrecargarme de tareas y actividades
+                  Poca experiencia usando git en trabajos en equipo. Pero con más experiencia usándola de manera individual.
+                </ListGroup.Item>
+                <ListGroup.Item>
+                  Tendencia a querer realizar muchas actividades en "simultaneo"
                 </ListGroup.Item>
                 <ListGroup.Item>
                   Autoexigencia al momento de elaborar y concretar proyectos
                 </ListGroup.Item>
-                <ListGroup.Item className="text-light">.</ListGroup.Item>
+                <ListGroup.Item>
+                  En proceso continuo de deconstrucción
+                </ListGroup.Item>
               </ListGroup>
             </Card.Text>
           </Card>
