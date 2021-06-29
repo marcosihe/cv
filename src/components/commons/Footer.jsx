af9f6c8f0d588b9f10e708260ce34e6055140e52
marcosihe <marcosherrera.ib@gmail.com>
2021-06-29 15:25:39 -0300

    Se agregaron iconos en el footer.

--- src/components/commons/Footer.jsx
+++ src/components/commons/Footer.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
+import React, { Fragment } from "react";
+import styles from './Footer.module.css'
 
 export const Footer = () => {
-    return (
-        <div className="text-center text-light bg-dark py-4 my-5">
-            {/* Github y linkedin iconos */}
-            <p>Marcos Ezequiel Herrera</p>
+  return (
+    <Fragment>
+      <div className="bg-dark py-4 my-5">
+        <div className="d-flex justify-content-center">
+        <a target="_blank" rel="noreferrer" href="https://github.com/marcosihe">
+          <img src={process.env.PUBLIC_URL + "github-icon.svg"} alt="Logo de github" className={styles.logo} />
+        </a>
+        <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/marcoseherrera/"><img src={process.env.PUBLIC_URL + "linkedin-icon.svg"} alt="Logo de Linkedin" className={styles.logo}/></a>
         </div>
-    )
-}
+        <p className="text-center text-light pt-2">Marcos Ezequiel Herrera</p>
+      </div>
+   
+    </Fragment>
+  );
+};
